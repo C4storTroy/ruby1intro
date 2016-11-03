f3763b11607e414aa247e0b1a02862e974de89c5
Lucio Moraes <luciomoraes@gmail.com>
2016-11-03 23:55:52 +1300

    first commit

--- ex1/big_or_small.rb
+++ ex1/big_or_small.rb
@@ -1,7 +1,30 @@
+#Studying ruby
+
 puts "Welcome for a new game"
 puts "What is your name?"
 name = gets
-puts
-puts
-puts "Lets start? "
-puts name
+puts "\n\n"
+puts "Lets start? " + name
+puts "Let's try a number between 0 and 200..."
+secret_number = 175
+max_try = 5
+
+for try in 1..max_try
+  puts "Try a number " + try.to_s + " de " + max_try.to_s
+  puts "First try: "
+  kick=gets
+  puts "Have you find? You tried: " + kick + "\n"
+
+  right = secret_number == kick.to_i
+  maior = secret_number > kick.to_i
+  if right
+    puts "Right"
+    break
+  else
+    if maior
+      puts "number is bigger"
+    else
+      puts "its not so big"
+    end
+  end
+end
